56b4d02c92de2c86109741e7ea1ec4476bd83188
if-not-null-get <zivan15934@gmail.com>
2018-12-05 14:50:36 +0100

    enable security

--- src/main/java/com/startup/loginservice/config/WebSecurityConfig.java
+++ src/main/java/com/startup/loginservice/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.startup.loginservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private BasicAuthHandler basicAuthHandler;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}1993").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin1Pass").roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
